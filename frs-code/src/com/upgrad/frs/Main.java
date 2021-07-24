685fa26d634c2e9604672b1adcb3a8598a77ab31
Ajay Singh <a.panwar48656@gmail.com>
2021-07-25 02:21:03 +0530

    added printTicketDetails method inside the Main class

--- frs-code/src/com/upgrad/frs/Main.java
+++ frs-code/src/com/upgrad/frs/Main.java
@@ -2,11 +2,20 @@ package com.upgrad.frs;
 
 
 public class Main {
+    public static void getTicketDetails(Ticket ticket){
+        System.out.println("PNR Number: " + ticket.getPnr());
+    }
+
     public static void main(String[] args) {
         Passenger p = new Passenger("Ajay", "+919875621257", "ajaysingh@mymail.com", "CP Street", "New Delhi", "Delhi");
         System.out.println("Passenger Count: " + p.getPassengerCount());
 
         Flight f = new Flight();
-        f.getFlightDetails();
+
+        RegularTicket t1 = new RegularTicket("Delhi", "Chennai", f, p);
+        getTicketDetails(t1);
+
+        TouristTicket t2 = new TouristTicket("Delhi", "Andaman", f, p);
+        getTicketDetails(t2);
     }
 }
