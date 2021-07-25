62d9fac75dc6375756ae8e1c7ccabf4f1aad9426
Ajay Singh <a.panwar48656@gmail.com>
2021-07-25 14:29:23 +0530

    Added cancelTicket method and updated getFlightDuration method in Ticket class

--- frs-code/src/com/upgrad/frs/Main.java
+++ frs-code/src/com/upgrad/frs/Main.java
@@ -1,21 +1,30 @@
 package com.upgrad.frs;
 
 
+import java.text.ParseException;
+
 public class Main {
     public static void getTicketDetails(Ticket ticket){
         System.out.println("PNR Number: " + ticket.getPnr());
     }
 
-    public static void main(String[] args) {
-        Passenger p = new Passenger("Ajay", "+919875621257", "ajaysingh@mymail.com", "CP Street", "New Delhi", "Delhi");
-        System.out.println("Passenger Count: " + p.getPassengerCount());
+    public static void main(String[] args) throws ParseException {
+        Passenger p1 = new Passenger("Ajay", "+919875621257", "ajaysingh@mymail.com", "CP Street", "New Delhi", "Delhi");
+        System.out.println("Passenger Count: " + p1.getPassengerCount());
 
         Flight f = new Flight();
+        System.out.println(f.getFlightDetails());
 
-        RegularTicket t1 = new RegularTicket("Delhi", "Chennai", f, p);
+        RegularTicket t1 = new RegularTicket("Delhi", "Chennai", f, p1);
         getTicketDetails(t1);
+        System.out.println("Flight Duration : " + t1.getFlightDuration());
 
-        TouristTicket t2 = new TouristTicket("Delhi", "Andaman", f, p);
+        
+        Passenger p2 = new Passenger("Raman", "+918965621257", "ramansangha@mymail.com", "Ludo Street", "New Delhi", "Delhi");
+        System.out.println("Passenger Count: " + p2.getPassengerCount());
+
+        TouristTicket t2 = new TouristTicket("Delhi", "Andaman", f, p2);
         getTicketDetails(t2);
+        System.out.println("Flight Duration : " + t2.getFlightDuration());
     }
 }
--- frs-code/src/com/upgrad/frs/Ticket.java
+++ frs-code/src/com/upgrad/frs/Ticket.java
@@ -1,5 +1,8 @@
 package com.upgrad.frs;
 
+import java.text.*;
+import java.util.*;
+
 public abstract class Ticket {
     private int pnr;
     private static int count = 56451216;
@@ -14,14 +17,13 @@ public abstract class Ticket {
         count++;
         this.from = from;
         this.to = to;
-        this.departureDateTime = "21 August 2021 8:30 p.m";
-        this.arrivalDatetime = "22 August 2021 8.00 a.m";
+        this.departureDateTime = "21 Aug 2021 20:30:00 p.m";
+        this.arrivalDatetime = "22 Aug 2021 8:00:00 a.m";
         this.seatNumber = "12A";
         this.price = 2000.95f;
         this.flight = flight;
         this.passenger = passenger;
         flight.incrementBookingCounter();
-        System.out.println(flight.getFlightDetails());
     }
 
     public int getPnr() {
@@ -52,8 +54,46 @@ public abstract class Ticket {
         return this.price;
     }
 
-    public int getFlightDuration(){
-        return 5;
+    public String getFlightDuration() throws ParseException {
+        int noOfDays = Integer.parseInt(this.arrivalDatetime.substring(0,2)) - Integer.parseInt(this.departureDateTime.substring(0,2));
+        String timeArrival = this.arrivalDatetime.substring(12,21);
+        String timeDeparture = this.departureDateTime.substring(12, 21);
+
+        // Creating a SimpleDateFormat object to parse time in the format HH:MM:SS
+        SimpleDateFormat simpleDateFormat
+                = new SimpleDateFormat("HH:mm:ss");
+
+        // Parsing the Time Period
+        Date date1 = simpleDateFormat.parse(timeArrival);
+        Date date2 = simpleDateFormat.parse(timeDeparture);
+
+        // Calculating the difference in milliseconds
+        long differenceInMilliSeconds
+                = Math.abs(date2.getTime() - date1.getTime());
+
+        // Calculating the difference in Hours
+        long differenceInHours
+                = (differenceInMilliSeconds / (60 * 60 * 1000)) % 24;
+
+        // Calculating the difference in Minutes
+        long differenceInMinutes
+                = (differenceInMilliSeconds / (60 * 1000)) % 60;
+
+        // Calculating the difference in Seconds
+        long differenceInSeconds
+                = (differenceInMilliSeconds / 1000) % 60;
+
+        if(noOfDays != 0){
+            return (int)differenceInHours + " Hours " + (int)differenceInMinutes + " Minutes "+ (int)differenceInSeconds + " Seconds ";
+        }
+        return noOfDays + " Days " + differenceInHours + " Hours " + differenceInMinutes + " Minutes "+ differenceInSeconds + " Seconds ";
+    }
+
+    public void cancelTicket(String str){
+        if(str.equalsIgnoreCase("cancel")){
+            cancelled = true;
+        }
+        cancelled = false;
     }
 
     public String checkStatus(){
